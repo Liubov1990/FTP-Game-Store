54bd988abad492bbb50312e3b76fc7b03d142b5f
Liuba <1990yavorska@gmail.com>
2022-02-14 14:48:00 +0200

    fix: set debounser on search btn

--- src/components/SortPanel/index.jsx
+++ src/components/SortPanel/index.jsx
@@ -18,6 +18,21 @@ function SortPanel() {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  function debounce(funk, timeout = 800){
+    let timer;
+    return (...args) => {
+      if (!timer) {
+        funk.apply(this, args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = undefined;
+      }, timeout);
+    };
+  }
+
+  const peventedMultiRequest = debounce(() => dispatch((getGamesListAsync())));
+
   return (
     <Paper elevation={0} square>
       <div className={classes.searchOptions}>
@@ -30,7 +45,7 @@ function SortPanel() {
         <Button
           variant="contained"
           classes={{ root: classes.buttonRoot }}
-          onClick={() => dispatch(getGamesListAsync())}
+          onClick={peventedMultiRequest}
         >
           Sort
         </Button>
