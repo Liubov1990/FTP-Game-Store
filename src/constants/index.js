42889181fc94512e3945f03e42f037419497b55c
Liuba <1990yavorska@gmail.com>
2022-08-01 17:19:40 +0300

    fix: deleted slash in const API_LINK_FREETOGAME

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export * from "./routes";
 
-export const API_LINK_FREETOGAME = "https://www.freetogame.com/api/";
+export const API_LINK_FREETOGAME = "https://www.freetogame.com/api";
 export const API_LINK = "https://www.giantbomb.com/api";
