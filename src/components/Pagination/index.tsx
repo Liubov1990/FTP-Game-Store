3294b84a33851fe08813c17624f621a1792da15c
Liuba <1990yavorska@gmail.com>
2022-09-06 14:22:12 +0300

    feat: added typescript for Pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React, { ChangeEvent, ReactElement } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // matrial-ui
 import Pagination from "@material-ui/lab/Pagination";
 import { makeStyles } from "@material-ui/core/styles";
-// actions
+// redux
+import { RootState } from "../../redux/store";
 import { setPage } from "../../redux/actions/imagesSearchActions";
 // styles
 import { paginationStyles } from "./styles";
 
 const useStyles = makeStyles(paginationStyles);
 
-function PaginationComponent() {
+function PaginationComponent(): ReactElement {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  const { page, totalPages } = useSelector(state => state.imagesSearchReducer);
+  const { page, totalPages } = useSelector((state: RootState): RootState["imagesSearchReducer"] => state.imagesSearchReducer);
 
-  const onPageChange = (event, page) => {
+  const onPageChange = (event: ChangeEvent<unknown>, page: number) => {
     dispatch(setPage(page));
   };
 
   return (
-    <div >
+    <div>
       <Pagination
         onChange={onPageChange}
         page={page}
