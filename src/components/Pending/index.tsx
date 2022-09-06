a40c02a11bca6239cfc825076627073a834e9da6
Liuba <1990yavorska@gmail.com>
2022-09-06 12:07:35 +0300

    refactor: added ClassNameMap and replaced extension of styles.js to styles.ts in Pending component

--- src/components/Pending/index.tsx
+++ src/components/Pending/index.tsx
@@ -2,13 +2,14 @@ import React, { ReactElement } from "react";
 // material-ui
 import { CircularProgress } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { ClassNameMap } from "@material-ui/core/styles/withStyles";
 // styles
 import { styles } from "./styles";
 
 const useStyles = makeStyles(styles);
 
 function Pending(): ReactElement {
-  const classes = useStyles();
+  const classes: ClassNameMap<string> = useStyles();
 
   return (
     <div className={classes.loadingWrapper}>
