cc22791e0482a9dac57a80eebb066f21af11e8c8
Liuba <1990yavorska@gmail.com>
2022-11-07 16:10:00 +0200

    refactor: deleted redundant logic

--- src/components/GameRequirements/tests/GameRequirements.test.tsx
+++ src/components/GameRequirements/tests/GameRequirements.test.tsx
@@ -1,35 +1,11 @@
-import React, { ReactElement } from "react";
-import { render } from "@testing-library/react";
-import { StoreProviderOverride } from "../../../App";
-import { MemoryRouter } from "react-router-dom";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
+import React from "react";
 import GameRequirements from "..";
 import { GAME_REQUIREMENTS_TEST_CASES } from "./assertions";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 
-interface IWrapperProps {
-  children?: ReactElement;
-  defaultStoreSource: {};
-}
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
 const initialStoreSource = {
   gamePageReducer: { gameDetails: {} }
 };
-
-const Wrapper = ({ children, defaultStoreSource = initialStoreSource }: IWrapperProps): ReactElement => {
-  const store = mockStore(defaultStoreSource);
-  store.dispatch = jest.fn();
-  return (
-    <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </StoreProviderOverride>
-  );
-};
-
-const renderWithWrapper = (ui: ReactElement, options?: any) =>
-  render(ui, { wrapper: props => <Wrapper {...props} {...options?.wrapperProps} />, ...options });
 
 describe("GameRequirements", () => {
   afterEach(() => {
@@ -37,7 +13,9 @@ describe("GameRequirements", () => {
   });
 
   test("should render component", () => {
-    const { getByText } = renderWithWrapper(<GameRequirements />);
+    const { getByText } = renderWithWrapper(<GameRequirements />, {
+      wrapperProps: { defaultStoreSource: initialStoreSource }
+    });
     expect(getByText(/Minimum system requirements/i)).toBeInTheDocument();
   });
 
