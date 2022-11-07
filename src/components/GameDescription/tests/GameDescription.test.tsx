bb26bf123bb96cfa8e26fcef5fde02670651d427
Liuba <1990yavorska@gmail.com>
2022-11-07 16:01:45 +0200

    refactor: deleted redundant logic

--- src/components/GameDescription/tests/GameDescription.test.tsx
+++ src/components/GameDescription/tests/GameDescription.test.tsx
@@ -1,33 +1,16 @@
-import React, { ReactElement } from "react";
-import { render } from "@testing-library/react";
-import { StoreProviderOverride } from "../../../App";
-import { MemoryRouter } from "react-router-dom";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
+import React from "react";
 import GameDescription from "..";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 
-interface wrapperProps {
-  children?: ReactElement;
-}
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-const store = mockStore({
+const initialStoreSource = {
   gamePageReducer: { gameDetails: { title: "test-game-title" }, similarGames: [], status: "NOT_ASKED" }
-});
-store.dispatch = jest.fn();
-
-const Wrapper = ({ children }: wrapperProps): ReactElement => {
-  return (
-    <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </StoreProviderOverride>
-  );
 };
 
 describe("GamePageReducer", () => {
   test("should render component", () => {
-    const { getByText } = render(<GameDescription />, { wrapper: Wrapper });
+    const { getByText } = renderWithWrapper(<GameDescription />, {
+      wrapperProps: { defaultStoreSource: initialStoreSource }
+    });
     const gameDescriptionComponent = getByText("test-game-title");
     expect(gameDescriptionComponent).toBeInTheDocument();
   });
