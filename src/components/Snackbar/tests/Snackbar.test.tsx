4c33fc849b24eaec8c40459ba9bd34465280633d
Liuba <1990yavorska@gmail.com>
2022-11-08 11:37:45 +0200

    refactor: deleted redundant logic

--- src/components/Snackbar/tests/Snackbar.test.tsx
+++ src/components/Snackbar/tests/Snackbar.test.tsx
@@ -1,46 +1,31 @@
-import React, { ReactElement } from "react";
-import { render } from "@testing-library/react";
-import { StoreProviderOverride } from "../../../App";
-import { MemoryRouter } from "react-router-dom";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
-import Snackbar from "..";
+import React from "react";
 import userEvent from "@testing-library/user-event";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 import { clearSnackbarFailure } from "../../../redux/actions/snackbarActions";
+import Snackbar from "..";
 
-interface IWrapperProps {
-  children?: ReactElement;
-}
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-const store = mockStore({
+const initialStoreSource = {
   snackbarReducer: { failureSnackbarOpen: true }
-});
-store.dispatch = jest.fn();
-
-const Wrapper = ({ children }: IWrapperProps): ReactElement => {
-  return (
-    <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </StoreProviderOverride>
-  );
 };
+
 describe("Snackbar", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   test("should render component", () => {
-    const { getByRole } = render(<Snackbar />, { wrapper: Wrapper });
+    const { getByRole } = renderWithWrapper(<Snackbar />, { wrapperProps: { defaultStoreSource: initialStoreSource } });
     expect(getByRole(/alert/i)).toBeInTheDocument();
   });
-  
+
   test("should call clearSnackbarFailure on handleClose", () => {
-    const { getByLabelText } = render(<Snackbar />, { wrapper: Wrapper });
+    const mockedDispatch = jest.fn();
+    const { getByLabelText } = renderWithWrapper(<Snackbar />, {
+      wrapperProps: { defaultStoreSource: initialStoreSource, defaultDispatch: mockedDispatch }
+    });
     const closeSnackbarBtn = getByLabelText(/Close/i);
     expect(closeSnackbarBtn).toBeVisible();
     userEvent.click(closeSnackbarBtn);
-    expect(store.dispatch).toHaveBeenCalledWith(clearSnackbarFailure());
+    expect(mockedDispatch).toHaveBeenCalledWith(clearSnackbarFailure());
   });
 });
