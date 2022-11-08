3280381daef686835cd6a414681e89bf565cae59
Liuba <1990yavorska@gmail.com>
2022-11-08 10:16:41 +0200

    fix: changed incorrect import; refactor: deleted redundant logic

--- src/components/GlanceGameInfo/tests/GlanceGameInfo.test.tsx
+++ src/components/GlanceGameInfo/tests/GlanceGameInfo.test.tsx
@@ -1,33 +1,16 @@
-import React, { ReactElement } from "react";
-import { render } from "@testing-library/react";
-import { StoreProviderOverride } from "../../../App";
-import { MemoryRouter } from "react-router-dom";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
-import GameGlanceInfo from "..";
+import React from "react";
+import GlanceGameInfo from "..";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 
-interface wrapperProps {
-  children?: ReactElement;
-}
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-const store = mockStore({
+const initialStoreSource = {
   gamePageReducer: { gameDetails: {}, similarGames: [], status: "NOT_ASKED" }
-});
-store.dispatch = jest.fn();
-
-const Wrapper = ({ children }: wrapperProps): ReactElement => {
-  return (
-    <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </StoreProviderOverride>
-  );
 };
 
 describe("GlanceGameInfo", () => {
   test("should render component", () => {
-    const { getByText } = render(<GameGlanceInfo />, { wrapper: Wrapper });
+    const { getByText } = renderWithWrapper(<GlanceGameInfo />, {
+      wrapperProps: { defaultStoreSource: initialStoreSource }
+    });
     expect(getByText(/Short description:/i)).toBeInTheDocument();
   });
 });
