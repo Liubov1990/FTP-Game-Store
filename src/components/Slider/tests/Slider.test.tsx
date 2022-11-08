2780f0f6f88d30833e21c0249cb4b5c9321c83b8
Liuba <1990yavorska@gmail.com>
2022-11-08 11:23:41 +0200

    refactor: deleted redundant logic

--- src/components/Slider/tests/Slider.test.tsx
+++ src/components/Slider/tests/Slider.test.tsx
@@ -1,39 +1,11 @@
-import React, { ReactElement, useState as useStateMock } from "react";
-import { render } from "@testing-library/react";
-import { StoreProviderOverride } from "../../../App";
-import { MemoryRouter } from "react-router-dom";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
+import React, { useState as useStateMock } from "react";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 import Slider from "..";
 
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
   useState: jest.fn()
 }));
-
-interface IWrapperProps {
-  children?: ReactElement;
-  defaultStoreSource: {};
-}
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-const initialStoreSource = {
-  imagesSearchReducer: { randomImagesList: [], page: 1, status: "NOT_ASKED" }
-};
-
-const Wrapper = ({ children, defaultStoreSource = initialStoreSource }: IWrapperProps): ReactElement => {
-  const store = mockStore(defaultStoreSource);
-  store.dispatch = jest.fn();
-  return (
-    <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </StoreProviderOverride>
-  );
-};
-
-const renderWithWrapper = (ui: ReactElement, options?: any) =>
-  render(ui, { wrapper: props => <Wrapper {...props} {...options?.wrapperProps} />, ...options });
 
 describe("ThumbnailSlider", () => {
   const setState = jest.fn();
