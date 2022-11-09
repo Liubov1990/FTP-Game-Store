565b4a42d8ce27c8d8b950312af6ac21f0c91fc3
Liuba <1990yavorska@gmail.com>
2022-11-09 14:21:29 +0200

    refactor: deleted comment

--- src/helpers/wrapper.tsx
+++ src/helpers/wrapper.tsx
@@ -1,35 +1,3 @@
-// import React, { ReactElement } from "react";
-// import { render } from "@testing-library/react";
-// import { StoreProviderOverride } from "../App";
-// import { MemoryRouter } from "react-router-dom";
-// import thunk from "redux-thunk";
-// import configureStore from "redux-mock-store";
-
-// interface IWrapperProps {
-//   children?: ReactElement;
-//   defaultStoreSource: {};
-//   defaultDispatch: jest.Mock;
-// }
-
-// const Wrapper = ({ children, defaultStoreSource, defaultDispatch = jest.fn() }: IWrapperProps): ReactElement => {
-//   const middlewares = [thunk];
-//   const mockStore = configureStore(middlewares);
-//   const store = mockStore(defaultStoreSource);
-//   store.dispatch = defaultDispatch;
-//   return (
-//     <StoreProviderOverride store={store}>
-//       <MemoryRouter>{children}</MemoryRouter>
-//     </StoreProviderOverride>
-//   );
-// };
-
-// export const renderWithWrapper = (ui: ReactElement, options?: any) =>
-//   render(ui, { wrapper: props => <Wrapper {...props} {...options?.wrapperProps} />, ...options });
-
-
-
-
-//////////////////////////////////////////////////////
 import React, { ReactElement } from "react";
 import { render } from "@testing-library/react";
 import { StoreProviderOverride } from "../App";
