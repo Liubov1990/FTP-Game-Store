c9b8968bc8ee84673360153ea7f44641b26ccf74
Liuba <1990yavorska@gmail.com>
2022-11-09 14:18:51 +0200

    refactor: deleted redundant logic; added initial prop value for 'wrapper' defaultStoreSource

--- src/components/Header/tests/Header.test.tsx
+++ src/components/Header/tests/Header.test.tsx
@@ -1,29 +1,10 @@
-import { render } from "@testing-library/react";
-import { ReactElement } from "react";
-import { MemoryRouter } from "react-router-dom";
+import React from "react";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 import Header from "../index";
-
-interface IBrowserWrapperprops {
-  children?: ReactElement;
-  pathname?: string;
-}
-
-interface IOptionProps {
-  wrapperProps?: {
-    pathname?: string;
-  };
-}
-
-const BrowserWrapper = ({ children, pathname = "/" }: IBrowserWrapperprops): ReactElement => {
-  return <MemoryRouter initialEntries={[{ pathname }]}>{children}</MemoryRouter>;
-};
-
-const renderWithWrapper = (ui: ReactElement, options: IOptionProps) =>
-  render(ui, { wrapper: props => <BrowserWrapper {...props} {...options.wrapperProps} />, ...options });
 
 describe("Header", () => {
   test("should match snapshot", () => {
-    const { container } = renderWithWrapper(<Header />, {});
+    const { container } = renderWithWrapper(<Header />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
--- src/helpers/wrapper.tsx
+++ src/helpers/wrapper.tsx
@@ -1,3 +1,35 @@
+// import React, { ReactElement } from "react";
+// import { render } from "@testing-library/react";
+// import { StoreProviderOverride } from "../App";
+// import { MemoryRouter } from "react-router-dom";
+// import thunk from "redux-thunk";
+// import configureStore from "redux-mock-store";
+
+// interface IWrapperProps {
+//   children?: ReactElement;
+//   defaultStoreSource: {};
+//   defaultDispatch: jest.Mock;
+// }
+
+// const Wrapper = ({ children, defaultStoreSource, defaultDispatch = jest.fn() }: IWrapperProps): ReactElement => {
+//   const middlewares = [thunk];
+//   const mockStore = configureStore(middlewares);
+//   const store = mockStore(defaultStoreSource);
+//   store.dispatch = defaultDispatch;
+//   return (
+//     <StoreProviderOverride store={store}>
+//       <MemoryRouter>{children}</MemoryRouter>
+//     </StoreProviderOverride>
+//   );
+// };
+
+// export const renderWithWrapper = (ui: ReactElement, options?: any) =>
+//   render(ui, { wrapper: props => <Wrapper {...props} {...options?.wrapperProps} />, ...options });
+
+
+
+
+//////////////////////////////////////////////////////
 import React, { ReactElement } from "react";
 import { render } from "@testing-library/react";
 import { StoreProviderOverride } from "../App";
@@ -7,18 +39,19 @@ import configureStore from "redux-mock-store";
 
 interface IWrapperProps {
   children?: ReactElement;
-  defaultStoreSource: {};
-  defaultDispatch: jest.Mock;
+  defaultStoreSource: Record<string, any>;
+  defaultDispatch?: jest.Mock;
+  pathname?: string;
 }
 
-const Wrapper = ({ children, defaultStoreSource, defaultDispatch = jest.fn() }: IWrapperProps): ReactElement => {
+const Wrapper = ({ children, defaultStoreSource = {}, defaultDispatch = jest.fn(), pathname = "/" }: IWrapperProps): ReactElement => {
   const middlewares = [thunk];
   const mockStore = configureStore(middlewares);
   const store = mockStore(defaultStoreSource);
   store.dispatch = defaultDispatch;
   return (
     <StoreProviderOverride store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
+      <MemoryRouter initialEntries={[pathname && { pathname }]}>{children}</MemoryRouter>
     </StoreProviderOverride>
   );
 };
