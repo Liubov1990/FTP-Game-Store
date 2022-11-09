0ec4a6c1206e476cff15b97840adb1912dc9c8ef
Liuba <1990yavorska@gmail.com>
2022-11-09 14:31:46 +0200

    refactor: deleted redundant logic

--- src/components/Layout/tests/Layout.test.tsx
+++ src/components/Layout/tests/Layout.test.tsx
@@ -1,35 +1,16 @@
-import { render } from "@testing-library/react";
-import { ReactElement } from "react";
-import { MemoryRouter } from "react-router-dom";
+import React from "react";
+import { renderWithWrapper } from "../../../helpers/wrapper";
 import Layout from "../index";
-
-interface IBrowserWrapperprops {
-  children?: ReactElement;
-  pathname?: string;
-}
-
-interface IOptionProps {
-  wrapperProps?: {
-    pathname?: string;
-  };
-}
-
-const BrowserWrapper = ({ children, pathname = "/" }: IBrowserWrapperprops): ReactElement => {
-  return <MemoryRouter initialEntries={[{ pathname }]}>{children}</MemoryRouter>;
-};
-
-const renderWithWrapper = (ui: ReactElement, options: IOptionProps) =>
-  render(ui, { wrapper: props => <BrowserWrapper {...props} {...options.wrapperProps} />, ...options });
 
 describe("Layout", () => {
   test("should match snapshot", () => {
-    const { container } = renderWithWrapper(<Layout />, {});
+    const { container } = renderWithWrapper(<Layout />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test("should call window.scrollTo()", () => {
     window.scrollTo = jest.fn();
-    renderWithWrapper(<Layout />, {});
+    renderWithWrapper(<Layout />);
     expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
   });
 });
