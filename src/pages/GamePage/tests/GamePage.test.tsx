bcc799d30fc69ca6189b36748090826b00adb769
Liuba <1990yavorska@gmail.com>
2022-11-17 15:34:16 +0200

    fix: bug with warning appearance during test 'should render component' execution

--- src/pages/GamePage/tests/GamePage.test.tsx
+++ src/pages/GamePage/tests/GamePage.test.tsx
@@ -9,21 +9,22 @@ describe("GamePage", () => {
 
   test("should render component", () => {
     const storeSource = { gamePageReducer: { gameDetails: { title: "test-title" }, status: "SUCCESS" } };
-    const { getByText } = renderWithWrapper(<GamePage />, { wrapperProps: { defaultStoreSource: storeSource } });
-    expect(getByText(/Short description/i)).toBeInTheDocument();
+    renderWithWrapper(<GamePage />, { wrapperProps: { defaultStoreSource: storeSource } });
+    const gameInfo = document.querySelector("[class*='gameInfo']") as HTMLElement;
+    expect(gameInfo).toBeInTheDocument();
   });
 
   test("should return 'Error", () => {
     const storeSource = { gamePageReducer: { gameDetails: {}, status: "FAILURE" } };
     renderWithWrapper(<GamePage />, { wrapperProps: { defaultStoreSource: storeSource } });
-    const errElement = document.querySelector(".makeStyles-error-53") as HTMLElement;
+    const errElement = document.querySelector("[class^='makeStyles-error']") as HTMLElement;
     expect(errElement).toBeInTheDocument();
   });
 
   test("should return no content", () => {
     const storeSource = { gamePageReducer: { gameDetails: {}, status: "NOT_ASKED" } };
     renderWithWrapper(<GamePage />, { wrapperProps: { defaultStoreSource: storeSource } });
-    const errElement = document.querySelector(".makeStyles-error-53") as HTMLElement;
+    const errElement = document.querySelector("[class^='makeStyles-error']") as HTMLElement;
     expect(errElement).not.toBeInTheDocument();
   });
 });
