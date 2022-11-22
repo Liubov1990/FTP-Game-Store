fa349ab42eb247769ed62c155121ba143c1b3ea2
Liuba <1990yavorska@gmail.com>
2022-11-22 11:49:34 +0200

    fix: resolved issue with CORS errors during 'Giantbomb API' requests

--- src/constants/apiLinks.ts
+++ src/constants/apiLinks.ts
@@ -1,2 +1,2 @@
-export const API_LINK_GIANT_BOMB = "https://proxy.cors.sh/https://www.giantbomb.com/api";
+export const API_LINK_GIANT_BOMB = "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api";
 export const API_LINK_RAPID = "https://free-to-play-games-database.p.rapidapi.com/api";
